819e5301477b4378da7dcc1694f40d1c75521671
varshini00111 <varshinigummadi2004@gmail.com>
2026-05-03 13:37:38 +0530

    Update application.py

--- application.py
+++ application.py
@@ -1,14 +1,7 @@
-"""
-This script runs the FlaskWebProject application using a development server.
-"""
-
-from os import environ
 from FlaskWebProject import app
 
-if __name__ == '__main__':
-    HOST = environ.get('SERVER_HOST', 'localhost')
-    try:
-        PORT = int(environ.get('SERVER_PORT', '5555'))
-    except ValueError:
-        PORT = 5555
-    app.run(HOST, PORT, ssl_context='adhoc')
+# This file is only for Azure to detect the app
+# Do NOT use app.run() here for Azure deployment
+
+if __name__ == "__main__":
+    app.run()
